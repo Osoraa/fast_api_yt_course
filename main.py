35dd3542fa7d40230a800f4a76e4812d3ffc364d
d3vn0n <sochi98.sn@gmail.com>
2023-09-13 02:42:44 +0100

    Retrieve posts endpoint/route/path operation

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello World from FastApi"}
+
+
+# Get Posts endpoint
+@app.get("/posts")
+def get_posts():
+    return {"data": "Empty posts"}
