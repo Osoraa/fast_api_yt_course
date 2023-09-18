b7b3bc649ca018bd43e7b344d253f733713500e4
d3vn0n <sochi98.sn@gmail.com>
2023-09-18 11:15:57 +0100

    fastapi.params.Body vs Pydantic post routes

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
-
+class Post(BaseModel):
+    title: str
+    content: str
+    
+    
 # Root route
 @app.get("/")
 def root():
@@ -13,3 +19,17 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {"data": "Empty posts"}
+
+
+# First post request
+@app.post("/create_post")
+def create_post(body: dict = Body(...)):
+    print(body)
+    return {"msg": f"Successfully created post with title {body['Title']}"}
+
+
+# Second post request
+@app.post("/create_post_2")
+def create_post(body:Post):
+    print(body)
+    return {"msg": f"Successfully created post2 with title {body.title}"}
