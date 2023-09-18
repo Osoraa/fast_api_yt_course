ef812e1fbe16d145d5f7b7dbefc8998d443723a2
d3vn0n <sochi98.sn@gmail.com>
2023-09-18 17:07:59 +0100

    Persist posts

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 class Post(BaseModel):
+    """Base Post class"""
+
     title: str
     content: str
     publish: bool=True
     location: Optional[str]=None
     
-    
+posts = []
+
+
 # Root route
 @app.get("/")
 def root():
@@ -20,18 +25,20 @@ def root():
 # Get Posts endpoint
 @app.get("/posts")
 def get_posts():
-    return {"data": "Empty posts"}
+    return {"data": posts}
 
 
 # First post request
-@app.post("/create_post")
-def create_post(body: dict = Body(...)):
+@app.post("/posts")
+def create_post(body: dict=Body(...)):
     print(body)
     return {"msg": f"Successfully created post with title {body['Title']}"}
 
 
 # Second post request
 @app.post("/create_post_2")
-def create_post(body:Post):
-    print(body)
-    return {"msg": f"Successfully created post2 with title {body.title}"}
+def create_post_2(body: Post):
+    posts.append(dict(body))
+    print(posts[0])
+    
+    # return {"msg": f"Successfully created post2 with title {body.title}"}
