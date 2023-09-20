72016c86ebf95562a769de50c2c0b1beb30c447f
d3vn0n <sochi98.sn@gmail.com>
2023-09-20 02:06:51 +0100

    Get a single Post

--- main.py
+++ main.py
@@ -6,44 +6,83 @@ from uuid import uuid4
 
 app = FastAPI()
 
+
 class Post(BaseModel):
     """Base Post class"""
 
     title: str
     content: str
-    publish: bool=True
-    location: Optional[str]=None
-    
-posts = []
+    publish: bool = True
+    location: Optional[str] = None
 
 
-# Root route
+posts = [
+    {
+        'title': 'Second post in Postman',
+        'content': 'Testing Optionals and Defaults',
+        'publish': True,
+        'location': None,
+        'id': 92
+    },
+    {
+        'title': 'Placeholder post',
+        'content': 'Basic Defaults',
+        'publish': False,
+        'location': "Lagos",
+        'id': 90
+    }
+]
+
+
+def find_post(id=int) -> dict:
+    """Retrieve post"""
+
+    for post in posts:
+        if post["id"] == id:
+            return post
+
+
 @app.get("/")
 def root():
+    """Root route"""
+
     return {"message": "Hello World from FastApi"}
 
 
-# Get Posts endpoint
 @app.get("/posts")
 def get_posts():
+    """Get Posts endpoint"""
+
     return {"data": posts}
 
 
-# First post request
 @app.post("/posts")
-def create_post(body: dict=Body(...)):
+def create_post(body: dict = Body(...)):
+    """First post request"""
+
     print(body)
     return {"msg": f"Successfully created post with title {body['Title']}"}
 
 
-# Second post request
 @app.post("/create_post_2")
 def create_post_2(body: Post):
-    body = dict(body)
-    
+    """Second post request"""
+
+    data = dict(body)
+
     # Use the UUID package for the post ID
-    body["id"] = uuid4().time
-    posts.append(body)
-    print(posts[0])
-    
-    return {"msg": f"Successfully created post2 with title {body['title']}"}
+    data["id"] = uuid4().time
+    posts.append(data)
+    print(data)
+
+    return {"msg": f"Successfully created post2 with title {body.title}"}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    """Route to get a single Post"""
+
+    post = find_post(id)
+    print(post)
+
+    return {"data": post}
